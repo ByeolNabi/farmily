157ac3ca02fa21bbfd8d41f4fe96b43357cbd8f6
korotic <korotic@naver.com>
2026-01-23 13:01:43 +0900

    hotfix: 업적 추가 ,영어 수정
    업적 추가했고 영어로 변경했습니다

---
+++ FE/Android/Farmily/app/src/main/java/com/d101/farmily/data/model/AchievInfo.kt
@@ -0,0 +1,6 @@
+package com.d101.farmily.data.model
+
+data class AchievInfo(
+    val name : String,
+    val description : String,
+)
---
+++ FE/Android/Farmily/app/src/main/java/com/d101/farmily/ui/component/AchievBox.kt
@@ -0,0 +1,66 @@
+package com.d101.farmily.ui.component
+
+import android.content.Context
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.d101.farmily.data.model.AchievInfo
+
+@Composable
+fun AchievBox(
+    context : Context,
+    item: AchievInfo,
+    modifier: Modifier = Modifier
+) {
+
+    val resourceId = remember(item.description) {
+        context.resources.getIdentifier(
+            item.description,      // 파일 이름 (확장자 제외)
+            "drawable",    // 리소스 폴더명
+            context.packageName
+        )
+    }
+
+
+    Column(modifier = modifier
+        .padding(vertical = 16.dp)
+        .padding(horizontal = 8.dp)
+        //.background(Color.Magenta)
+        ,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(12.dp)
+    ) {
+        Box(
+            modifier = Modifier.size(60.dp)
+                //.background(item.backColor, CircleShape)
+            ,
+            contentAlignment = Alignment.Center
+        ) {
+
+            Image(
+                painter = painterResource(id = resourceId),
+                contentDescription = null,
+                //tint = item.color,
+                modifier = Modifier
+                    .size(60.dp)
+            )
+        }
+        Column (
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            //Text(item.name, style = MaterialTheme.typography.labelSmall, color = Color.Gray)
+            Text(item.name, style = MaterialTheme.typography.titleSmall)
+        }
+    }
+}
