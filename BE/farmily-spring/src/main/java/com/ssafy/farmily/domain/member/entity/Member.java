170708615b6708f30587e1038560d5b664cc45a1
parksoyeon <psy9881@naver.com>
2026-01-28 16:30:59 +0900

    config: Member.java에 Column = id 명시

--- BE/farmily-spring/src/main/java/com/ssafy/farmily/domain/member/entity/Member.java
+++ BE/farmily-spring/src/main/java/com/ssafy/farmily/domain/member/entity/Member.java
@@ -13,6 +13,7 @@ public class Member {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
     private Long id;
 
     @Column(nullable = false, unique = true, length = 255)
